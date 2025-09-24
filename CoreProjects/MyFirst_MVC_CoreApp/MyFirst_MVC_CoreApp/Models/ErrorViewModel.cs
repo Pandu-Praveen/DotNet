bcd8483b3b527c88b827ac2d382cb515b26433cc
PRAVEEN S <130171193+Pandu-Praveen@users.noreply.github.com>
2025-09-24 21:34:28 +0530

    Delete CoreProjects/MyFirst_MVC_CoreApp/MyFirst_MVC_CoreApp directory

--- CoreProjects/MyFirst_MVC_CoreApp/MyFirst_MVC_CoreApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirst_MVC_CoreApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
